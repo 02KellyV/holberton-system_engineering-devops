5a710466e3dec4f38d851e147971348f05108615
02KellyV <900@holbertonschool.com>
2020-03-26 05:12:54 +0000

    puppet nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,10 +3,10 @@ package {'nginx':
   ensure => 'present',
 }->
 exec {'Holberton':
-  command => '/bin/echo "Holberton School" > /var/www/html/index.nginx-debian.html',
+  command => '/bin/echo "Holberton School" > /var/www/html/index.html',
   returns => [0,2]
 }->
-exec {'sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/youtu.be/IBH4g_ua5es\/;\\n\\t}/" /etc/nginx/sites-enabled/default':
+exec {'sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://youtu.be/IBH4g_ua5es permanent;" /etc/nginx/sites-available/default':
   path => '/usr/bin:/usr/sbin:/bin',
 }->
 exec {'run':
