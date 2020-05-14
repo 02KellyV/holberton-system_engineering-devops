75223a480e981043201d9a12e6f495da457eeea9
02KellyV <900@holbertonschool.com>
2020-05-14 02:25:43 +0000

    Recursive Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,10 +7,10 @@ import requests
 
 def top_ten(subreddit):
     """first 10 hot posts listed"""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    agt = {"User-Agent": "linux:1:v1.1 (by /u/heimer_r)"}
+    URL = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    Headers = {"User-Agent": "Custom"}
     payload = {"limit": "10"}
-    hot = requests.get(url, headers=agt, params=payload, allow_redirects=False)
+    hot = requests.get(URL, headers=Headers, params=payload, allow_redirects=False)
     if hot.status_code != 200:
         print("None")
     else:
---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Recursive function that queries the Reddit API
+   Parses the title of all hot articles
+   Prints a sorted count of given keywords:
+       case-insensitive
+       delimited by spaces
+       Javascript should count as javascript
+       but java should not
+"""
+from requests import get
+from sys import argv
+
+
+def count_words(subreddit, word_list, after="", counter={}, t=0):
+    """count and print a sorted list"""
+    if t == 0:
+        for word in word_list:
+            counter[word] = 0
+    headers = {"User-Agent": "Custom"}
+    json = get("https://api.reddit.com/r/{}/hot?after={}".
+               format(subreddit, after), headers=headers).json()
+    try:
+        key = json['data']['after']
+        parent = json['data']['children']
+        for obj in parent:
+            for word in counter:
+                counter[word] += obj['data']['title'].lower().split(
+                    ' ').count(word.lower())
+        if key is not None:
+            count_words(subreddit, word_list, key, counter, 1)
+        else:
+            res = sorted(counter.items(), key=lambda i: i[1], reverse=True)
+            for key, value in res:
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+    except Exception:
+        return None
