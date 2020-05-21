9196bd2e5113f7a90e7a16c3508fa65e766862aa
02KellyV <900@holbertonschool.com>
2020-05-21 00:57:31 +0000

    puppet again, path bin, command for fix 500 issue

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+#Apache is returning a 500 error, Fix Issue
+exec {'automate_fix_500Issue':
+  path    => ['/bin'],
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php && service apache2 reload"
+}
